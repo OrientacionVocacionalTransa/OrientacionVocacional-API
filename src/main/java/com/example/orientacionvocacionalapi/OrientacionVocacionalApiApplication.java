d0db93d322c1d41a0e2c6f67d39e3d3917d59247
espinozaje <jeisson12aaron@gmail.com>
2024-09-26 15:35:44 -0500

    first commit

---
+++ src/main/java/com/example/orientacionvocacionalapi/OrientacionVocacionalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.orientacionvocacionalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientacionVocacionalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientacionVocacionalApiApplication.class, args);
+    }
+
+}
