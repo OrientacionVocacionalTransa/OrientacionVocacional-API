c1a5576199181e8fc829a9e3ae350a20971b1d56
Yuliana Cacho <ycachoq1@upao.edu.pe>
2024-09-28 15:06:57 -0500

    Insertar carrera

--- src/main/java/com/example/orientacionvocacionalapi/Controller/CarreraController.java
+++ src/main/java/com/example/orientacionvocacionalapi/Controller/CarreraController.java
@@ -20,10 +20,26 @@ public class CarreraController {
     @Autowired
     private UbicacionRepository ubicacionRepository;
 
+
     @PostMapping("/insertarubi")
     public ResponseEntity<Ubicacion> agregarUbicacion(@RequestBody Ubicacion ubicacion) {
         Ubicacion nuevaUbicacion = ubicacionRepository.save(ubicacion);
         return ResponseEntity.ok(nuevaUbicacion);
+    }
+
+    @PostMapping("/insertarcarrera")
+    public ResponseEntity<Carrera> agregarCarrera(@RequestParam String nombreCarrera, @RequestParam Long ubicacionId) {
+        Ubicacion ubicacion = ubicacionRepository.findById(ubicacionId).orElse(null);
+        if (ubicacion == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        Carrera carrera = new Carrera();
+        carrera.setNombre(nombreCarrera);
+        carrera.setUbicacion(ubicacion);
+
+        Carrera nuevaCarrera = carreraRepository.save(carrera);
+        return ResponseEntity.ok(nuevaCarrera);
     }
 
     @GetMapping("/porubicacion/{ubicacionId}")
