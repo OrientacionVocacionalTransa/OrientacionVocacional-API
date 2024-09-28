c5449b21f04eab0a9243a5f905fa94d2258e1835
espinozaje <jeisson12aaron@gmail.com>
2024-09-27 23:43:56 -0500

    OVT-2 Visualizar Resultados

--- src/main/java/com/example/orientacionvocacionalapi/Controller/VocationalController.java
+++ src/main/java/com/example/orientacionvocacionalapi/Controller/VocationalController.java
@@ -1,9 +1,15 @@
 package com.example.orientacionvocacionalapi.Controller;
+import com.example.orientacionvocacionalapi.model.Question;
+import com.example.orientacionvocacionalapi.model.VocationalTest;
 import com.example.orientacionvocacionalapi.model.entity.Pregunta;
 import com.example.orientacionvocacionalapi.repository.PreguntaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/vocational-test")
@@ -12,6 +18,12 @@ public class VocationalController {
     @Autowired
     private PreguntaRepository preguntaRepository;
 
+    @PostMapping("/submit")
+    public ResponseEntity<ApiResponse> submitVocationalTest(@RequestBody VocationalTest test){
+        String result = calculateTestResult(test);
+        ApiResponse apiResponse = new ApiResponse(result);
+        return ResponseEntity.ok(apiResponse);
+    }
 
     @GetMapping("/questions")
     public List<Pregunta> getQuestions(){
@@ -20,6 +32,27 @@ public class VocationalController {
         return preguntas;
     }
 
+    private String calculateTestResult(VocationalTest test){
+        Map<String, Integer> areaScores = new HashMap<>();
 
+        for (Question question: test.getQuestions()){
+            if (question.getSelectedOption().getScore()==1){
+                String area = question.getArea();
+                areaScores.put(area, areaScores.getOrDefault(area, 0)+1);
+            }
+        }
+
+        String recommendedArea = "";
+        int maxScore = 0;
+
+        for (Map.Entry<String, Integer> entry : areaScores.entrySet()){
+            if(entry.getValue()>maxScore){
+                maxScore = entry.getValue();
+                recommendedArea = entry.getKey();
+            }
+        }
+
+        return "Area recomendada: "+ recommendedArea;
+    }
 }
 
