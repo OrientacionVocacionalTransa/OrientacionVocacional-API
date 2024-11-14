2eb5f14a89049642f228d680385770d24929ac07
Lourdes Angulo Renteria <langulor2@upao.edu.pe>
2024-11-14 03:14:41 -0500

    Endpoint para reprogramar y cancelar asesoria

--- src/main/java/com/example/orientacionvocacionalapi/Controller/AdvisoryController.java
+++ src/main/java/com/example/orientacionvocacionalapi/Controller/AdvisoryController.java
@@ -6,10 +6,7 @@ import com.example.orientacionvocacionalapi.service.impl.AdvisoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/advisories")
@@ -22,5 +19,17 @@ public class AdvisoryController {
     public ResponseEntity<Advisory> createAdvisory(@Validated @RequestBody AdvisoryDTO advisoryDTO) {
         Advisory advisory = advisoryService.createAdvisory(advisoryDTO);
         return ResponseEntity.ok(advisory);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Advisory> updateAdvisory(@Validated @RequestBody AdvisoryDTO advisoryDTO) {
+        Advisory advisory = advisoryService.updateAdvisory(advisoryDTO.getId(), advisoryDTO);
+        return ResponseEntity.ok(advisory);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteAdvisory(@RequestParam Integer advisoryId) {
+        advisoryService.deleteAdvisory(advisoryId);
+        return ResponseEntity.noContent().build();
     }
 }
