fbee25ff9b5af58fcc5fd25e120d9749cc7c684b
PieroSanchez <pierosancas@gmail.com>
2024-11-14 01:24:27 -0500

    Insercion data DTO

--- src/main/java/com/example/orientacionvocacionalapi/dto/PurchaseDTO.java
+++ src/main/java/com/example/orientacionvocacionalapi/dto/PurchaseDTO.java
@@ -1,4 +1,17 @@
 package com.example.orientacionvocacionalapi.dto;
 
+import com.example.orientacionvocacionalapi.model.enums.PaymentStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class PurchaseDTO {
+    private Integer id;
+    private Float total;
+    private LocalDateTime createAt;
+    private PaymentStatus paymentStatus;
+    private String user;
+    private List<PurchaseItemDTO> items;
 }
