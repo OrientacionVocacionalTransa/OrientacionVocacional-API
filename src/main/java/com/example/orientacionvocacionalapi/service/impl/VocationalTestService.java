03c6f97cacb8c46067843c3080dbfe80ac2b2849
Yamir Vargas Alvitres <yvargasa2@upao.edu.pe>
2024-11-14 02:21:51 -0500

    Agregación entidades area y testresult

--- src/main/java/com/example/orientacionvocacionalapi/service/impl/VocationalTestService.java
+++ src/main/java/com/example/orientacionvocacionalapi/service/impl/VocationalTestService.java
@@ -1,4 +1,20 @@
 package com.example.orientacionvocacionalapi.service.impl;
 
+import com.example.orientacionvocacionalapi.model.entity.Career;
+import com.example.orientacionvocacionalapi.model.entity.Question;
+import com.example.orientacionvocacionalapi.model.entity.User;
+import com.example.orientacionvocacionalapi.model.entity.VocationalTest;
+import com.example.orientacionvocacionalapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class VocationalTestService {
+
 }
